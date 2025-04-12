8e671ef0fb62419db9ed0eac11aacb99513ef519
Steve Stavropoulos <steve@math.upatras.gr>
2025-04-13 01:59:51 +0300

    .

--- examples/scripts/sft.py
+++ examples/scripts/sft.py
@@ -89,7 +89,7 @@ if __name__ == "__main__":
         attn_implementation=model_config.attn_implementation,
         torch_dtype=model_config.torch_dtype,
         use_cache=False if training_args.gradient_checkpointing else True,
-        device_map=get_kbit_device_map() if quantization_config is not None else None,
+        device_map=get_kbit_device_map() if quantization_config is not None else "auto",
         quantization_config=quantization_config,
     )
     training_args.model_init_kwargs = model_kwargs
